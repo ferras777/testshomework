4574353245ea9dc8ba1159f3506f586ddd7226a6
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-08-26 16:30:15 +0300

    Changes PositiveTest

---
+++ src/test/java/Simple/NegativeTests.java
@@ -0,0 +1,34 @@
+package Simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    void test0() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Tag("smoke")
+    void test2() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(false);
+    }
+}
--- src/test/java/WebForm/TestBase.java
+++ src/test/java/WebForm/TestBase.java
@@ -1,9 +1,8 @@
-package Tests;
+package WebForm;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
