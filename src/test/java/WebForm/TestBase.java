617c86807ee9f45919dfd0ca8ee8b814fc6f4d85
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-08-26 16:43:47 +0300

    Changes TestBase ..

--- src/test/java/WebForm/TestBase.java
+++ src/test/java/WebForm/TestBase.java
@@ -2,7 +2,9 @@ package WebForm;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -19,6 +21,13 @@ public class TestBase {
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
+    }
+
+    @AfterEach
+    public  void tearDown(){
+        Attach.screenshotAs ("Last screenshot");
+        Attach.pageSource ();
+        Attach.browserConsoleLogs ();
     }
 
 }
