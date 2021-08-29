692f94475c997a18972738f9706f412032a6730a
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-08-30 00:14:56 +0300

    Added changes..

---
+++ src/test/java/Tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,50 @@
+package Tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("properties")
+public class SystemPropertiesTests {
+    @Test
+    void test1() {
+        System.out.println("[test1] Browser: " + System.getProperty("browser"));
+    }
+
+    @Test
+    void readNullFromPropertyTest() {
+        String value = System.getProperty("our_property");
+        System.out.println(value);
+        // null
+    }
+
+    @Test
+    void readDefaultFromPropertyTest() {
+        String value = System.getProperty("our_property", "default_value");
+        System.out.println(value);
+        // default_value
+    }
+
+    @Test
+    void readWithTerminalFromPropertyTest() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+
+    }
+
+    @Test
+    void setPropertyTest() {
+
+        System.out.println(System.getProperty("browser"));
+        System.setProperty("browser", "chrome");
+
+        System.out.println(System.getProperty("browser"));
+
+
+    }
+}
