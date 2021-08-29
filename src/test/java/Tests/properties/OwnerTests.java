46e23440e29ca45a2ed775ccdccd5ed35711aedb
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-08-29 23:31:26 +0300

    Added Owner..

---
+++ src/test/java/Tests/properties/OwnerTests.java
@@ -0,0 +1,26 @@
+package Tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+@Tag("properties")
+public class OwnerTests {
+
+    @Test
+    void readCredentialsTest() {
+        String login = "credentials.login()";
+        String password = "credentials.password()";
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("I try to login as %s with password %s", login, password);
+
+        System.out.println(message);
+
+    }
+
+}
+
